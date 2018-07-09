f49c7ebb79c736da819028d5a07ea537a7686f4c
tamas.czerjak <tamas.czerjak@icellmobilsoft.hu>
2018-07-09 20:02:47 +0200

    feat: create basic calculator method adn test for it

--- date-calculator.js
+++ date-calculator.js
@@ -1,5 +1,15 @@
 class DateCalculator {
+    constructor(workingDays, workingHours) {
+        this.workingDays = workingDays;
+        this.workingHours = workingHours;
+    }
 
+    calculateDueDate(submitDate, turnAroundTime) {
+        const submitDateObject = new Date(submitDate);
+        if (isNaN(submitDateObject)) {
+            throw new Error('submitDate is invalid');
+        }
+    }
 }
 
-module.exports =  DateCalculator;
+module.exports = DateCalculator;
--- test/date-calculator.test.js
+++ test/date-calculator.test.js
@@ -1,8 +1,22 @@
 const assert = require('assert');
 const DateCalculator = require('../date-calculator');
 
-describe('DateCalculator', function() {
-  it('is an object', function() {
-    assert.ok(DateCalculator instanceof Object);
+describe('calculator', function() {
+  const calculator = new DateCalculator(
+    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    { start: 9, end: 17 }
+  );
+
+  it('is an instance of DateCalculator', function() {
+    assert.ok(calculator instanceof DateCalculator);
+  });
+
+  describe('calculateDueDate', function() {
+    it('returns a string', function() {
+      assert.equal(typeof calculator.calculateDueDate('1999.05.25 15:55', 18), 'string');
+    });
+    it('throws an error if the date is invalid', function() {
+      assert.throws(calculator.calculateDueDate.bind(calculator, 'asdasdasd', 18));
+    });
   });
 });
