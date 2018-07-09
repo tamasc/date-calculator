745204128fb6344374fabfb5ca94930d697c0544
tamas.czerjak <tamas.czerjak@icellmobilsoft.hu>
2018-07-09 22:36:15 +0200

    feat: check submission validity

--- date-calculator.js
+++ date-calculator.js
@@ -2,14 +2,46 @@ class DateCalculator {
     constructor(workingDays, workingHours) {
         this.workingDays = workingDays;
         this.workingHours = workingHours;
+        this.workingHoursADay = this.workingDays.end - this.workingDays.start;
     }
 
     calculateDueDate(submitDate, turnAroundTime) {
         const submitDateObject = new Date(submitDate);
+        this.checkSubmissionValidity(submitDateObject);
+        return '';
+    }
+
+    checkSubmissionValidity(submitDateObject) {
         if (isNaN(submitDateObject)) {
             throw new Error('submitDate is invalid');
         }
+        const numberOfDay = submitDateObject.getDay();
+        const hours = submitDateObject.getHours();
+        const minutes = submitDateObject.getMinutes();
+        if (
+            this.isNotWorkingDay(numberOfDay) ||
+            this.isTooEarly(hours) ||
+            this.isTooLate(hours, minutes)
+        ) {
+            throw new Error('submitDate is out of the working hours');
+        }
+    }
+
+    isNotWorkingDay(numberOfDay) {
+        const day = DateCalculator.daysOfTheWeek[numberOfDay];
+        return !this.workingDays.includes(day);
+    }
+
+    isTooEarly(submitHour) {
+        return this.workingHours.start > submitHour;
+    }
+
+    isTooLate(submitHour, submitMinutes) {
+        const isTheEndOfTheWorkingDay = this.workingHours.end === submitHour && submitMinutes === 0;
+        return !isTheEndOfTheWorkingDay && this.workingHours.end <= submitHour;
     }
 }
+
+DateCalculator.daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 module.exports = DateCalculator;
--- test/date-calculator.test.js
+++ test/date-calculator.test.js
@@ -18,5 +18,38 @@ describe('calculator', function() {
     it('throws an error if the date is invalid', function() {
       assert.throws(calculator.calculateDueDate.bind(calculator, 'asdasdasd', 18));
     });
+    it('throws an error if the day is not a working day', function() {
+      assert.throws(calculator.calculateDueDate.bind(calculator, '2018.07.08 15:55', 18));
+    });
+    it('throws an error if the time after the working hours', function() {
+      assert.throws(calculator.calculateDueDate.bind(calculator, '2018.07.09 17:01', 18));
+    });
+    it('throws an error if the time before the working hours', function() {
+      assert.throws(calculator.calculateDueDate.bind(calculator, '2018.07.09 7:59', 18));
+    });
+  });
+
+  describe('isNotWorkingDay', function() {
+    it('returns a boolean', function() {
+      assert.equal(typeof calculator.isNotWorkingDay(5), 'boolean');
+    });
+    it('Friday is a working day', function() {
+      assert.equal(calculator.isNotWorkingDay(5), false);
+    });
+    it('Sunday is not a working day', function() {
+      assert.ok(calculator.isNotWorkingDay(0));
+    });
+  });
+
+  describe('isTooLate', function() {
+    it('returns a boolean', function() {
+      assert.equal(typeof calculator.isTooLate(17, 0), 'boolean');
+    });
+    it('17:00 is not too late', function() {
+      assert.equal(calculator.isTooLate(17, 0), false);
+    });
+    it('17:01 is too late', function() {
+      assert.ok(calculator.isTooLate(17, 1));
+    });
   });
 });
